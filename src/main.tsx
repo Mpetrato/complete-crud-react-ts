2a42e2a41b51c7b70ce3e3cf77c44fef70e4187b
Matheus Petrato <matheuspetratomp@gmail.com>
2022-08-13 23:27:04 -0300

    Added Light Theme and provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
+import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
+import { LightTheme } from './shared/themes/Light'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <ThemeProvider theme={LightTheme}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ThemeProvider>
 )
